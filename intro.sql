b2a7beaad05668472939590310550cacae994563
FaizulOsman <faizulosmanrabby@gmail.com>
2023-08-08 11:27:48 +0600

    first commit

---
+++ intro.sql
@@ -0,0 +1,136 @@
+-- ################## 31-1 (Create/Delete 2 Column with Relation) ##################
+
+-- Department Table
+-- Each Department has many employee
+CREATE TABLE Department(
+    deptID SERIAL PRIMARY KEY,
+    deptName VARCHAR(50)
+);
+
+INSERT INTO Department VALUES (1, 'IT');
+
+DELETE FROM Department WHERE deptID = 1;
+
+SELECT * FROM department;
+
+
+
+-- Employee Table
+-- Each Employee belongs to a department
+CREATE TABLE Employee(
+    empID SERIAL PRIMARY KEY,
+    empName VARCHAR(50) NOT NULL,
+    departmentID INT,
+    CONSTRAINT fk_constraint_dept
+        FOREIGN KEY (departmentID)
+        REFERENCES Department(deptID)
+);
+
+INSERT INTO Employee VALUES (1, 'Abul', 1);
+
+DELETE FROM Employee WHERE departmentID = 1;
+
+SELECT * FROM employee;
+
+
+
+-- ################## 31-2 (Insert/Update/Delete Multiple Data From Table) ##################
+CREATE TABLE courses(
+    course_id SERIAL NOT NULL,
+    course_name VARCHAR(255) NOT NULL,
+    description VARCHAR(255),
+    publication_date DATE
+);
+
+INSERT INTO courses (course_name, description, publication_date)
+VALUES 
+    ('PostgreSQL for Developers', 'A complete PostgreSQL for Developers', '2020-07-13'),
+    ('PostgreSQL Administration', 'A PostgreSQL Guide for Developers', NULL),
+    ('PostgreSQL High Performance', NULL, NULL),
+    ('PostgreSQL BootCamp', 'Learn PostgreSQL Via BootCamp', '2013-07-11'),
+    ('Mastering PostgreSQL', 'Mastering PostgreSQL in 21 Days', '2012-06-30');
+
+UPDATE courses SET
+    course_name = 'PostgreSQL For Advance',
+    description = 'Complete PostgreSQL Guideline'
+    WHERE course_id = 18;
+
+UPDATE courses SET
+    course_name = 'Advance',
+    description = 'Complete Guideline'
+    WHERE course_id > 2 AND course_id < 5;
+
+DELETE FROM courses WHERE course_id = 3 OR course_id = 5;
+
+SELECT * FROM courses;
+
+
+-- ################## 31-3 (Different Types of Filtering and Get Data) ##################
+CREATE TABLE IF NOT EXISTS departments (
+    deptID SERIAL PRIMARY KEY,
+    name TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS employees (
+    empID SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    email TEXT NOT NULL,
+    salary INTEGER NOT NULL,
+    joining_date DATE NOT NULL,
+    deptID INTEGER NOT NULL,
+    CONSTRAINT fk_deptID
+        FOREIGN KEY(deptID)
+        REFERENCES departments(deptID)
+);
+
+-- Professional way to get data (Don't use * to get all)
+SELECT email, name, joining_date FROM employees;
+
+-- It will show every data only 1 time (Duplicate data will show 1 time)
+SELECT DISTINCT name FROM employees;
+
+-- Filter data with condition
+SELECT * FROM employees WHERE salary > 50000 AND salary < 60000;
+
+-- Not equal (<>)
+SELECT * FROM employees WHERE email <> "oliver.watson@example.com";
+
+
+
+-- ################## 31-4 (Filter data by Limit and Offset) ##################
+
+-- Get Data By Name With Ascending/Descending order
+SELECT * FROM employees ORDER BY name DESC;
+
+-- limit 10 Offset 0 = first 10 data
+-- limit 10 Offset 1 = data 11 t0 20
+SELECT * FROM employees ORDER BY name ASC LIMIT 10 OFFSET 0;
+
+-- Get data highest salary
+SELECT * FROM employees ORDER BY salary DESC LIMIT 1;
+
+-- Get data by third highest salary
+SELECT * FROM employees ORDER BY salary DESC LIMIT 1 OFFSET 2;
+
+
+
+-- ################## 31-5 (Search data in PostgreSQL) ##################
+
+-- Get data by empID = (2, 3, 5)
+SELECT * FROM employees WHERE empID IN (2, 3, 5);
+
+-- Get data without empID = (2, 3, 5)
+SELECT * FROM employees WHERE empID NOT IN (2, 3, 5);
+
+-- Get data by where salary range (40000 - 50000)
+SELECT * FROM employees WHERE salary BETWEEN 40000 AND 50000;
+
+-- Search data (Case sensitive)
+SELECT * FROM employees WHERE name LIKE '%ily%';    -- Find like include (Emily, Lily)
+SELECT * FROM employees WHERE name LIKE 'Emily%';   -- Which will start by Emily
+SELECT * FROM employees WHERE name LIKE '__ily%';   -- Third position (Emily)
+SELECT * FROM employees WHERE name LIKE 'E%y';      -- Start with E end with y
+SELECT * FROM employees WHERE deptID IS null;
+
+
+
